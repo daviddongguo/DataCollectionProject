1460e3a86331742240e0e7193adcd0df014db199
daviddongguo <david.dong.guo@gmail.com>
2019-04-03 14:54:40 -0400

    [update] add a test method for testing bean

--- datacollection/src/main/java/io/moh/datacollection/controller/TestController.java
+++ datacollection/src/main/java/io/moh/datacollection/controller/TestController.java
@@ -3,12 +3,19 @@ package io.moh.datacollection.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.moh.datacollection.domain.entities.RowProfile;
+
 @RestController
 public class TestController {
 
 	@GetMapping("/test")
 	public String getTestString() {
 		return "success.";
+	}
+
+	@GetMapping("/test/row")
+	public RowProfile getRowProfile() {
+		return new RowProfile("It's a test row name.");
 	}
 
 }
