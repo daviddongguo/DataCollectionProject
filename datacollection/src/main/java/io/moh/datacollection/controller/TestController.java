ad8304630661b08d664c7e51cc3901be7e95348c
daviddongguo <david.dong.guo@gmail.com>
2019-04-03 15:17:11 -0400

    [chg] use @Pathvariable

--- datacollection/src/main/java/io/moh/datacollection/controller/TestController.java
+++ datacollection/src/main/java/io/moh/datacollection/controller/TestController.java
@@ -1,6 +1,7 @@
 package io.moh.datacollection.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.moh.datacollection.domain.entities.RowProfile;
@@ -13,9 +14,9 @@ public class TestController {
 		return "success.";
 	}
 
-	@GetMapping("/test/row")
-	public RowProfile getRowProfile() {
-		return new RowProfile("It's a test row name.");
+	@GetMapping("/test/row/{rowName}")
+	public RowProfile getRowProfile(@PathVariable String rowName) {
+		return new RowProfile(rowName);
 	}
 
 }
