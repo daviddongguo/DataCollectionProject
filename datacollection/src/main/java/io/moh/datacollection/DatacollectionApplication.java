bdad0f1a7204f7e567c8ead4731f4306cbc9bc75
daviddongguo <david.dong.guo@gmail.com>
2019-04-03 10:39:49 -0400

    [new]  download a new project generated by https://start.spring.io using web, devtools, jpa, h2

---
+++ datacollection/src/main/java/io/moh/datacollection/DatacollectionApplication.java
@@ -0,0 +1,13 @@
+package io.moh.datacollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatacollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatacollectionApplication.class, args);
+	}
+
+}
