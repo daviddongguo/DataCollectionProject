e311d96aa643d91e8f5a7fa91ce7cd0b5f9d19e1
daviddongguo <david.dong.guo@gmail.com>
2019-04-03 15:49:19 -0400

    [chg]  Enhancing POST Method to return correct HTTP Status Code and Location

--- datacollection/src/main/java/io/moh/datacollection/controller/TestController.java
+++ datacollection/src/main/java/io/moh/datacollection/controller/TestController.java
@@ -1,22 +1,46 @@
 package io.moh.datacollection.controller;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import io.moh.datacollection.domain.entities.RowProfile;
 
 @RestController
 public class TestController {
 
+	private List<RowProfile> rowsList = new ArrayList<>();
+
 	@GetMapping("/test")
 	public String getTestString() {
 		return "success.";
 	}
 
-	@GetMapping("/test/row/{rowName}")
+	@GetMapping("/test/rows/{rowName}")
 	public RowProfile getRowProfile(@PathVariable String rowName) {
 		return new RowProfile(rowName);
+	}
+
+	@GetMapping("/test/rows")
+	public List<RowProfile> getAllRow() {
+		return this.rowsList;
+	}
+
+	@PostMapping("/test/rows")
+	public ResponseEntity<Object> createRow(@RequestBody RowProfile row) {
+
+		rowsList.add(row);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(row.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
 	}
 
 }
