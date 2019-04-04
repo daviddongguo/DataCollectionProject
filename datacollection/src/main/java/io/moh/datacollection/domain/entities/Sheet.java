8229b03eca7d2178e117732e0687743e57089422
daviddongguo <david.dong.guo@gmail.com>
2019-04-04 10:29:09 -0400

    [chg] modify the fetchTYPE.EAGER

--- datacollection/src/main/java/io/moh/datacollection/domain/entities/Sheet.java
+++ datacollection/src/main/java/io/moh/datacollection/domain/entities/Sheet.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -27,7 +28,8 @@ public class Sheet {
 
 	private String name;
 
-	@OneToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH })
+	@OneToMany(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH,
+			CascadeType.REFRESH })
 	@JoinColumn(name = "sheet")
 	private Set<ColumnProfile> header;
 
