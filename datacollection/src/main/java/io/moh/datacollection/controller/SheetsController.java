97367c6c71c039d01dc527426edcb391505562dd
daviddongguo <david.dong.guo@gmail.com>
2019-04-04 10:26:56 -0400

    [chg] using @RestController for test sheet Controller

--- datacollection/src/main/java/io/moh/datacollection/controller/SheetsController.java
+++ datacollection/src/main/java/io/moh/datacollection/controller/SheetsController.java
@@ -5,34 +5,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.moh.datacollection.domain.entities.Sheet;
 import io.moh.datacollection.repository.SheetRepository;
 
-// @RestController
+@RestController
 // Instead using Spring Data REST to implement the controller
 public class SheetsController {
 
 	@Autowired
 	SheetRepository repository;
 
-	@GetMapping("/first")
+	@GetMapping("/test/first")
 	public String first() {
 		return "It's the first method.";
 	}
 
-	@GetMapping("/sheets")
+	@GetMapping("/test/test/sheets")
 	public Iterable<Sheet> getValues() {
 		return repository.findAll();
 	}
 
-	@PostMapping("/sheets")
+	@PostMapping("/test/sheets")
 	public Sheet addValue(@RequestBody Sheet value) {
 		Sheet dbValue = repository.save(value);
 		return dbValue;
 	}
 
-	@GetMapping("/sheets/{id}")
+	@GetMapping("/test/sheets/{id}")
 	public Sheet getValues(@PathVariable long id) {
 		return repository.findById(id).get();
 	}
