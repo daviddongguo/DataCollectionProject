5e230c91618f7a22dc40411c5b78f452537809b8
daviddongguo <david.dong.guo@gmail.com>
2019-04-04 10:28:24 -0400

    [update] using spring JpaRepository for every entity

--- datacollection/src/main/java/io/moh/datacollection/repository/LineRepository.java
+++ datacollection/src/main/java/io/moh/datacollection/repository/LineRepository.java
@@ -1,9 +1,11 @@
 package io.moh.datacollection.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import io.moh.datacollection.domain.entities.Line;
 
+@Repository
 public interface LineRepository extends JpaRepository<Line, Long> {
 
 }
--- datacollection/src/main/java/io/moh/datacollection/repository/SheetRepository.java
+++ datacollection/src/main/java/io/moh/datacollection/repository/SheetRepository.java
@@ -1,9 +1,11 @@
 package io.moh.datacollection.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import io.moh.datacollection.domain.entities.Sheet;
 
+@Repository
 public interface SheetRepository extends JpaRepository<Sheet, Long> {
 
 }
